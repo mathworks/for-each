9ce6cd5660fcad2cbc17dca10ac32c21e4e60a0d
Jeremy Hughes <jhughes@mathworks.com>
2023-01-06 15:07:42 -0500

    Simplify each.test.all using TestSuite

--- +each/+test/all.m
+++ +each/+test/all.m
@@ -1,13 +1,6 @@
-% Copyright 2014 The MathWorks, Inc.
+% Copyright 2014-2023 The MathWorks, Inc.
 % Runs all the tests in the each.test package
 
-if ~exist('matlab.unittest.TestCase','class')
-    error('Each:EarlierVersion','This test file requires the unit testing framework.')
-end
-
-me = meta.package.fromName('each.test');
-tests = { me.ClassList.Name };
-
-for test = each(tests)
-    run( feval( test ) );
-end
+import matlab.unittest.TestSuite
+suite = TestSuite.fromPackage("each.test");
+suite.run();
